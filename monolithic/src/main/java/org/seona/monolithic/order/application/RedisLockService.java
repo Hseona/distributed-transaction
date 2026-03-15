4e0838e83239fb34b4ff1474ae56a151a93800c9
seona <hseona28@gmail.com>
2026-03-15 13:10:34 +0900

    feat: 11. Lock 활용해서 주문로직이 한번만 수행되도록 변경

---
+++ monolithic/src/main/java/org/seona/monolithic/order/application/RedisLockService.java
@@ -0,0 +1,23 @@
+package org.seona.monolithic.order.application;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisLockService {
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public RedisLockService(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    // 락 점유용
+    public boolean tryLock(String lockKey, String lockValue) {
+        return this.stringRedisTemplate.opsForValue().setIfAbsent(lockKey, lockValue);
+    }
+
+    // 락 해제용
+    public void releaseLock(String lockKey) {
+        stringRedisTemplate.delete(lockKey);
+    }
+}
--- monolithic/src/main/java/org/seona/monolithic/order/controller/OrderController.java
+++ monolithic/src/main/java/org/seona/monolithic/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package org.seona.monolithic.order.controller;
 
 import org.seona.monolithic.order.application.OrderService;
+import org.seona.monolithic.order.application.RedisLockService;
 import org.seona.monolithic.order.application.dto.CreateOrderResult;
 import org.seona.monolithic.order.controller.dto.CreateOrderRequest;
 import org.seona.monolithic.order.controller.dto.CreateOrderResponse;
@@ -14,14 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrderController {
     private final OrderService orderService;
+    private final RedisLockService redisLockService;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService,  RedisLockService redisLockService) {
         this.orderService = orderService;
+        this.redisLockService = redisLockService;
     }
 
     @PostMapping("/place")
     public void placeOrder(@RequestBody PlaceOrderRequest request) throws InterruptedException {
-        orderService.placeOrder(request.toPlaceOrderCommand());
+        String key = "order:monolithic:" + request.orderId();
+        boolean acquiredLock = redisLockService.tryLock(key, request.orderId().toString());
+
+        if (!acquiredLock) {
+            throw new RuntimeException("락 획득에 실패했습니다.");
+        }
+
+        try {
+            orderService.placeOrder(request.toPlaceOrderCommand());
+        } finally {
+            redisLockService.releaseLock(key);
+        }
     }
 
     @PostMapping
@@ -29,4 +43,6 @@ public class OrderController {
         CreateOrderResult result = orderService.createOrder(request.toCreateOrderCommand());
         return new CreateOrderResponse(result.orderId());
     }
+
+
 }
